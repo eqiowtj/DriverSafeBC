90db08fae31426a3b7c1b0b3197062626bfb236c
Alice S <alices@Alices-MacBook-Pro.local>
2022-11-06 15:28:51 -0800

    sending sticker id to firebase

--- app/src/main/java/edu/northeastern/driversafebc/a7atyourservice/SendStickerActivity.java
+++ app/src/main/java/edu/northeastern/driversafebc/a7atyourservice/SendStickerActivity.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import edu.northeastern.driversafebc.R;
 import edu.northeastern.driversafebc.databinding.ActivitySendStickerBinding;
 
@@ -16,6 +19,10 @@ public class SendStickerActivity extends AppCompatActivity {
     private ActivitySendStickerBinding binding;
     private Spinner dropdown;
     private TextView stickerSelectedTextView;
+    private String currentSelectedSticker;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +35,17 @@ public class SendStickerActivity extends AppCompatActivity {
     public void selectStickerOnClick(View view) {
         Button button = (Button) view;
         stickerSelectedTextView.setText("Sticker " + button.getText() + " selected");
+        currentSelectedSticker = (String) button.getText();
+    }
+
+    //when pressed, save data to firebase
+    public void sendButtonOnClick(View view) {
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("StickerSentHistory");
+
+
+        //get the values: sender (current logged-in user), receiver, and sticker-id-to-send
+        StickerHelper helper = new StickerHelper("dummy-sender", "dummy-receiver", currentSelectedSticker);
+        reference.setValue(helper);
     }
 }
---
+++ app/src/main/java/edu/northeastern/driversafebc/a7atyourservice/StickerHelper.java
@@ -0,0 +1,39 @@
+package edu.northeastern.driversafebc.a7atyourservice;
+
+public class StickerHelper {
+    String sender, receiver, id;
+
+    public StickerHelper() {
+
+    }
+
+    public StickerHelper(String sender, String receiver, String id) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
